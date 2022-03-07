4592d6c6aa10a7ef4aa96495c3f6780dfa39e6f3
Thanh Tran <tt222kj@student.lnu.se>
2022-03-07 11:41:38 +0100

    templates

---
+++ diary/app.py
@@ -0,0 +1,158 @@
+from asyncio.windows_events import NULL
+from typing import List, Dict
+from flask import Flask, render_template, request, redirect, url_for, flash, session, g
+import mysql.connector
+import json
+from datetime import datetime
+import sys
+import functools
+from db import Database
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
+
+db_config = {
+    'user': 'root',
+    'password': 'root',
+    'host': '127.0.0.1',
+    'database': 'diary_app'
+}
+
+db = Database(**db_config)
+
+connection = mysql.connector.connect(**db_config)
+cursor = connection.cursor(dictionary=True)
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('login'))
+
+        return view(**kwargs)
+
+    return 
+
+@app.before_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = db.find_user_by_id(user_id)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        else:
+            if db.find_user_by_username(username) is not None:
+                error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.create_user(username, password)
+            return redirect(url_for('login'))
+
+        flash(error)
+    return render_template('register.html')
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        user = db.find_user_by_username(username)
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not user['password'] == password:
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['user_id']
+            return redirect(url_for('diaries'))
+
+        flash(error)
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+@app.route('/diaries', methods=('GET', 'POST'))
+def diaries():
+
+    if g.user is None:
+        return redirect(url_for('login'))
+
+    user_id = session.get('user_id')
+    if request.method == 'POST':
+        diary_name = request.form['diary_type']
+        db.create_diary(user_id, diary_name)
+    
+    diaries = db.find_diary_by_user_id(user_id)
+    return render_template('diaries.html', diaries = diaries)
+
+@app.route('/diary/<diary_id>', methods=('GET', 'POST'))
+def diary(diary_id):
+    if g.user is None:
+        return redirect(url_for('login'))
+
+    user_id = session.get('user_id')
+
+    diary = db.find_diary_by_diary_id(diary_id)
+
+    if diary['user_id'] != user_id:
+        return redirect(url_for('diaries'))
+
+    if request.method == 'POST':
+        story_name = request.form['story_name']
+        content = request.form['content']
+
+        story = db.find_story_by_name(story_name)
+
+        if story is None:
+            story_id = db.create_story(story_name)
+        else:
+            story_id = story['story_id']
+
+        db.create_diary_type(diary_id, story_id, NULL, NULL, NULL, NULL)
+
+    exercises = db.find_story_data_for_diary_by_story_id(diary_id)
+    return render_template('diary.html', diaries = diaries)
+
+@app.route('/story_detail')
+def story_detail():
+    if g.user is None:
+        return redirect(url_for('login'))
+
+    user_id = session.get('user_id')
+    story_detail = db.find_story_details_by_user_id(user_id)
+
+    return render_template('story_detail.html', stories = story_detail)
+
+@app.route('/diary_list')
+def diary_list():
+    records = db.find_all_diary()
+
+    return render_template('diary_list.html', records = records)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ diary/db.py
@@ -0,0 +1,85 @@
+import mysql.connector
+
+class Database:
+    def __init__(self, **kwargs):
+        self.connection = mysql.connector.connect(**kwargs)
+        self.cursor = self.connection.cursor(dictionary=True)
+
+    def find_user_by_id(self, user_id):
+        self.cursor.execute(f"SELECT * FROM user WHERE user_id = '{user_id}'")
+
+        return self.cursor.fetchone()
+
+    def find_user_by_username(self, username):
+        self.cursor.execute(f"SELECT * FROM user WHERE username = '{username}'")
+        
+        return self.cursor.fetchone()
+
+    def create_user(self, username, password):
+        self.cursor.execute(f"INSERT INTO user (username, password) VALUES ('{username}', '{password}')")
+        self.connection.commit()
+
+        return self.cursor.lastrowid
+
+    def create_diary(self, user_id, diary_type):
+        self.cursor.execute(f"INSERT INTO diaries VALUES (0, {user_id},'{diary_type}', NOW())")
+        self.connection.commit()
+
+        return self.cursor.lastrowid
+
+    def find_diary_by_user_id(self, user_id):
+        self.cursor.execute(f"SELECT * FROM diaries WHERE user_id = {user_id}")
+
+        return self.cursor.fetchall()
+    
+    def find_diary_by_diary_id(self, diary_id):
+        self.cursor.execute(f"SELECT * FROM diaries WHERE diary_id = {diary_id}")
+
+        return self.cursor.fetchone()
+
+    def find_story_by_name(self, story_name):
+        self.cursor.execute(f"SELECT * FROM story WHERE name = '{story_name}'")
+        return self.cursor.fetchone()
+    
+    def create_story(self, story_name, story_content):
+        self.cursor.execute(f"INSERT INTO exercise VALUES (0, '{story_name}', '{story_content}')")
+        self.connection.commit()
+
+        return self.cursor.lastrowid
+    
+    def create_diary_type(self, diary_id, story_id, name, description, day, process):
+        self.cursor.execute(f"INSERT INTO diary_type VALUES (0, {diary_id}, {story_id}, {name}, {description}, {day}, {process})")
+        self.connection.commit()
+
+        return self.cursor.lastrowid
+
+    def find_story_data_for_diary_by_story_id(self, story_id):
+        self.cursor.execute(("SELECT name, description "
+                            "FROM diary_type "
+                            "INNER JOIN story "
+                            "ON diary_type.story_id = story.story_id "
+                            f"WHERE story_id = {story_id}"))
+
+        return self.cursor.fetchall()
+
+    def find_story_details_by_user_id(self, user_id):
+        self.cursor.execute(("SELECT "
+                                "story.name AS name, "
+                                "ROUND(AVG(process), 1) AS avg_process, "
+                                "MAX(process) AS max_process, "
+                                "ROUND(AVG(working_day), 1) AS avg_working_day, "
+                                "SUM(working_day) AS total_working_day "
+                            "FROM diary_type "
+                            "INNER JOIN diaries "
+                                "ON diary_type.diary_id = diaries.diary_id "
+                            "INNER JOIN story "
+                                "ON diary_type.story_id = story.story_id "
+                            f"WHERE user_id = {user_id} "
+                            "GROUP BY story.name"))
+
+        return self.cursor.fetchall()
+ 
+    def find_all_diary(self):
+        self.cursor.execute("SELECT * FROM diary_list")
+
+        return self.cursor.fetchall()
